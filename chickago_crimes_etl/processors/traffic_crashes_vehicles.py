070490cc197717a21c4ae9c2517a44d703dcd666
NeonKirill <kirill.grim@gmail.com>
2024-05-20 23:21:35 +0200

    Added vehicle processing dimension

--- chickago_crimes_etl/processors/traffic_crashes_vehicles.py
+++ chickago_crimes_etl/processors/traffic_crashes_vehicles.py
@@ -10,4 +10,22 @@ class TrafficCrashesVehiclesCSVProcessor(TrafficCrashesCSVProcessor):
         return os.getenv("TRAFFIC_CRASHES_VEHICLES_SOURCE_PATH")
 
     def run_processing(self, data: pd.DataFrame, destination_path: str):
-        pass
+        self._populate_vehicle_dim_csv(data=data,
+                                       destination_path=destination_path)
+
+    def _populate_vehicle_dim_csv(self, data: pd.DataFrame, destination_path: str):
+        victims_agg_dim_filename = 'traffic_vehicle_dim.csv'
+        destination_path = os.path.join(destination_path, victims_agg_dim_filename)
+        # considering only car-based causes
+        data = data.loc[data['UNIT_TYPE'] == 'DRIVER']
+        # removing multiple-car accidents
+        data = data.drop_duplicates(subset=['CRASH_RECORD_ID'], keep=False)
+        data['VEHICLE_YEAR'] = data['VEHICLE_YEAR'].fillna(-1).astype(int)
+        vehicle_columns = {'CRASH_RECORD_ID': 'IdIncident',
+                           'MAKE': 'VehicleMake',
+                           'MODEL': 'VehicleModel',
+                           'VEHICLE_YEAR': 'VehicleYear',
+                           'VEHICLE_TYPE': 'VehicleType',
+                           'VEHICLE_USE': 'VehicleUse',
+                           'MANEUVER': 'Maneuver'}
+        self._populate_to_csv(data=data, destination_path=destination_path, columns_mapping=vehicle_columns)
